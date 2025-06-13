4bc14b88f684c619cebb9da83615c67b5d5beccd
AGNESH K <agnesh9025@gmail.com>
2025-06-13 20:50:41 +0530

    commit

--- src/main/java/com/example/todo/TaskController.java
+++ src/main/java/com/example/todo/TaskController.java
@@ -1,5 +1,8 @@
 package com.example.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -9,29 +12,42 @@ import java.util.List;
 public class TaskController {
 
     private final TaskRepository taskRepository;
-
+    @Autowired
     public TaskController(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
     @GetMapping
-    public List<Task> getTasks() {
-        return taskRepository.findAll(); // Returns list of your Task entity
+    public ResponseEntity<List<Task>> getTasks() {
+        List<Task> tasks = taskRepository.findAll();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
+
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task); // Saves task to DB
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+       Task newtask = taskRepository.save(task);
+       return new ResponseEntity<>(newtask, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
-        task.setId(id); // Sets the ID for update
-        return taskRepository.save(task); // Updates the task in DB
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskRepository.findById(id)
+                .map(existingTask -> {
+                    existingTask.setText(task.getText());
+                    Task updatedTask = taskRepository.save(existingTask);
+                    return new ResponseEntity<>(updatedTask, HttpStatus.OK);
+                })
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
-        taskRepository.deleteById(id); // Deletes the task by ID
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if(!taskRepository.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        taskRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
